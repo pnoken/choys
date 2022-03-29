d030ecb9b4f7f4b3ce52ad1d60cdc79de623b390
Peter Nortey <ecexpay@gmail.com>
2022-03-29 11:44:51 +0000

    corrected class for classname

--- components/preloader.js
+++ components/preloader.js
@@ -1,11 +1,11 @@
 const Preloader = () => {
   return (
-    <div class="flex items-center justify-center">
+    <div className="flex items-center justify-center">
       <div
-        class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
         role="status"
       >
-        <span class="visually-hidden">
+        <span className="visually-hidden">
           <img src="brand/choys.svg" alt="choys logo" />
         </span>
       </div>
