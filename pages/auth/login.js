2d1b9927db7e4dd00907077d239ea89f34328e8e
Peter Nortey <ecexpay@gmail.com>
2022-03-30 01:14:23 +0000

    class to classname in login

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -85,7 +85,7 @@ export default function Login() {
                     {loading ? (
                       <button
                         type="button"
-                        class="bg-blue-300 text-white active:bg-blue-600 text-sm font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        className="bg-blue-300 text-white active:bg-blue-600 text-sm font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                         disabled
                       >
                         Logging In...
