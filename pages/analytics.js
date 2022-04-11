3474b462baac0fbfd858955f12c613bb106e179f
Peter Nortey <ecexpay@gmail.com>
2022-04-11 09:57:11 +0000

    strava dATA

--- pages/analytics.js
+++ pages/analytics.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import axiosInstance from "../utils/axiosconfig";
 
 export default function Analytics() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
     const getAthleteActivities = async () => {
       axiosInstance
@@ -25,10 +25,14 @@ export default function Analytics() {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-8/12 px-1">{/* <CardAthleteData /> */}</div>
         <div className="w-full lg:w-4/12 px-1">
-          <div>{data.id}</div>
-          <div>{data.username}</div>
-          <div>{data.firstname}</div>
-          <div>{data.lastname}</div>
+          {/* <div>{data.id}</div> */}
+          {data.length > 0 ? (
+            <div>{data.distance}</div>
+          ) : (
+            <div>No Data found</div>
+          )}
+          {/* <div>{data.firstname}</div>
+          <div>{data.lastname}</div> */}
           {/* <CardAPI redirectURI={redirectURI} authenticated={authenticated} /> */}
         </div>
       </div>
