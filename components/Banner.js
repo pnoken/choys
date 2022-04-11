557108272344ae2bd22f5159e6f53d0a0ad9a9cc
Peter Nortey <ecexpay@gmail.com>
2022-04-11 15:17:13 +0000

    corrected error in banner

--- components/Banner.js
+++ components/Banner.js
@@ -15,16 +15,16 @@ export default function Example() {
             <p className="ml-3 font-medium text-white truncate">
               <span className="md:hidden">We announced a new product!</span>
               <span className="hidden md:inline">
-                Big news! We're excited to announce a brand new product.
+                Kindly verify your email address
               </span>
             </p>
           </div>
           <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
             <a
-              href="#"
+              href="/mail"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
             >
-              Learn more
+              Go to Main
             </a>
           </div>
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
