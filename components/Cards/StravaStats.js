5caa3a83b9303db0a0c43d7007ff128c4a6ed27c
Peter Nortey <ecexpay@gmail.com>
2022-04-12 01:52:35 +0000

    added dependency for get athlete activity

--- components/Cards/StravaStats.js
+++ components/Cards/StravaStats.js
@@ -16,7 +16,7 @@ export default function StravaCard() {
         });
     };
     getAthleteActivities();
-  }, []);
+  }, [data]);
   return (
     <section className="py-20 2xl:py-40 bg-blue-800">
       {data && data.length > 0 ? (
