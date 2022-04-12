0e41b75df613db8f9162d637a689182efc24d30f
Peter Nortey <ecexpay@gmail.com>
2022-04-12 01:30:28 +0000

    refactored strava card

--- components/Cards/StravaStats.js
+++ components/Cards/StravaStats.js
@@ -1,91 +1,118 @@
-export default function StravaCard({
-  distance,
-  average_speed,
-  average_heartrate,
-  location_country,
-}) {
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosconfig";
+
+export default function StravaCard() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getAthleteActivities = async () => {
+      axiosInstance
+        .get("/athlete/activities")
+        .then(function (response) {
+          console.log(response.data);
+          setData(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    getAthleteActivities();
+  }, []);
   return (
     <section className="py-20 2xl:py-40 bg-blue-800">
-      <div className="container px-4 mx-auto">
-        <div>
-          <div className="flex flex-wrap -mx-6 lg:-mx-14">
-            <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 lg:mb-0">
-              <div className="relative flex items-center justify-center mb-10 w-20 h-20">
-                {/* <img
+      {data && data.length > 0 ? (
+        data.map((stravadata, id) => {
+          const {
+            distance,
+            average_speed,
+            average_heartrate,
+            location_country,
+          } = stravadata;
+          return (
+            <div key={id} className="container px-4 mx-auto">
+              <div>
+                <div className="flex flex-wrap -mx-6 lg:-mx-14">
+                  <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 lg:mb-0">
+                    <div className="relative flex items-center justify-center mb-10 w-20 h-20">
+                      {/* <img
                   className="absolute inset-0 w-full h-full"
                   src="zospace-assets/lines/circle-chart.svg"
                   alt=""
                 /> */}
-                <p className="text-2xl font-bold text-white">
-                  <span>{distance}</span>
-                  <span className="text-base">m</span>
-                </p>
-              </div>
-              <div className="py-px mb-12 bg-gray-500"></div>
-              <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
-                Distance
-              </h3>
-              <p className="text-lg text-gray-200">
-                You covered a distanc of {distance} m
-              </p>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 lg:mb-0">
-              <div className="relative flex items-center justify-center mb-10 w-20 h-20">
-                {/* <img
+                      <p className="text-2xl font-bold text-white">
+                        <span>{distance}</span>
+                        <span className="text-base">m</span>
+                      </p>
+                    </div>
+                    <div className="py-px mb-12 bg-gray-500"></div>
+                    <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
+                      Distance
+                    </h3>
+                    <p className="text-lg text-gray-200">
+                      You covered a distanc of {distance} m
+                    </p>
+                  </div>
+                  <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 lg:mb-0">
+                    <div className="relative flex items-center justify-center mb-10 w-20 h-20">
+                      {/* <img
                   className="absolute inset-0 w-full h-full"
                   src="zospace-assets/lines/circle-chart-purple.svg"
                   alt=""
                 /> */}
-                <p className="text-2xl font-bold text-white">
-                  <span>{average_speed}</span>
-                  <span className="text-base">m/h</span>
-                </p>
-              </div>
-              <div className="py-px mb-12 bg-gray-500"></div>
-              <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
-                You covered an Average Speed of {average_speed}
-              </h3>
-              <p className="text-lg text-gray-200">Average Speed</p>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 md:mb-0">
-              <div className="relative flex items-center justify-center mb-10 w-20 h-20">
-                {/* <img
+                      <p className="text-2xl font-bold text-white">
+                        <span>{average_speed}</span>
+                        <span className="text-base">m/h</span>
+                      </p>
+                    </div>
+                    <div className="py-px mb-12 bg-gray-500"></div>
+                    <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
+                      You covered an Average Speed of {average_speed}
+                    </h3>
+                    <p className="text-lg text-gray-200">Average Speed</p>
+                  </div>
+                  <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14 mb-16 md:mb-0">
+                    <div className="relative flex items-center justify-center mb-10 w-20 h-20">
+                      {/* <img
                   className="absolute inset-0 w-full h-full"
                   src="zospace-assets/lines/circle-chart-green.svg"
                   alt=""
                 /> */}
-                <p className="text-2xl font-bold text-white">
-                  <span>{average_heartrate}</span>
-                  <span className="text-base">beats per min</span>
-                </p>
-              </div>
-              <div className="py-px mb-12 bg-gray-500"></div>
-              <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
-                Average Heartrate
-              </h3>
-              <p className="text-lg text-gray-200">Heartrate</p>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14">
-              <div className="relative flex items-center justify-center mb-10 w-20 h-20">
-                {/* <img
+                      <p className="text-2xl font-bold text-white">
+                        <span>{average_heartrate}</span>
+                        <span className="text-base">beats per min</span>
+                      </p>
+                    </div>
+                    <div className="py-px mb-12 bg-gray-500"></div>
+                    <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
+                      Average Heartrate
+                    </h3>
+                    <p className="text-lg text-gray-200">Heartrate</p>
+                  </div>
+                  <div className="w-full md:w-1/2 lg:w-1/4 px-6 lg:px-14">
+                    <div className="relative flex items-center justify-center mb-10 w-20 h-20">
+                      {/* <img
                   className="absolute inset-0 w-full h-full"
                   src="zospace-assets/lines/circle-chart-pink.svg"
                   alt=""
                 /> */}
-                <p className="text-2xl font-bold text-white">
-                  <span>{location_country}</span>
-                  <span className="text-base"></span>
-                </p>
+                      <p className="text-2xl font-bold text-white">
+                        <span>{location_country}</span>
+                        <span className="text-base"></span>
+                      </p>
+                    </div>
+                    <div className="py-px mb-12 bg-gray-500"></div>
+                    <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
+                      Location Country
+                    </h3>
+                    <p className="text-lg text-gray-200">Location</p>
+                  </div>
+                </div>
               </div>
-              <div className="py-px mb-12 bg-gray-500"></div>
-              <h3 className="mt-12 mb-8 text-lg font-bold font-heading text-white">
-                Location Country
-              </h3>
-              <p className="text-lg text-gray-200">Location</p>
             </div>
-          </div>
-        </div>
-      </div>
+          );
+        })
+      ) : (
+        <div>No Data found</div>
+      )}
     </section>
   );
 }
--- pages/analytics.js
+++ pages/analytics.js
@@ -1,7 +1,5 @@
 import withAuth from "../components/PrivateRoute";
 import Admin from "../components/Layout/Admin";
-import { useEffect, useState } from "react";
-import axiosInstance from "../utils/axiosconfig";
 import dynamic from "next/dynamic";
 import Preloader from "../components/preloader";
 
@@ -10,47 +8,11 @@ const StravaCard = dynamic(() => import("../components/Cards/StravaStats"), {
 });
 
 export default function Analytics() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const getAthleteActivities = async () => {
-      axiosInstance
-        .get("/athlete/activities")
-        .then(function (response) {
-          console.log(response.data);
-          setData(response.data);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-    getAthleteActivities();
-  }, []);
-
   return (
     <>
       <div className="flex flex-wrap">
         <div className="w-full px-1">
-          {data && data.length > 0 ? (
-            data.map((stravadata, id) => {
-              const {
-                distance,
-                average_speed,
-                average_heartrate,
-                location_country,
-              } = stravadata;
-              return (
-                <StravaCard
-                  key={id}
-                  distance={distance}
-                  average_speed={average_speed}
-                  average_heartrate={average_heartrate}
-                  location_country={location_country}
-                />
-              );
-            })
-          ) : (
-            <div>No Data found</div>
-          )}
+          <StravaCard />
         </div>
         <div className="w-full lg:w-4/12 px-1"></div>
       </div>
