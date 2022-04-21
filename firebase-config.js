5b8650417187ba2f1d1b4fde66e99f933f76ab80
Peter Nortey <ecexpay@gmail.com>
2022-04-21 07:56:47 +0000

    added env files

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getDatabase, ref, set } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBh9IniFiyQF99DHjOEZzvhaDRGCtJl6c",
-  authDomain: "choys-a2612.firebaseapp.com",
-  projectId: "choys-a2612",
-  storageBucket: "choys-a2612.appspot.com",
-  messagingSenderId: "89386478468",
-  appId: "1:89386478468:web:ff10f8b04fa20e00a4cbd1",
-  measurementId: "G-8VE717DM4E",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
