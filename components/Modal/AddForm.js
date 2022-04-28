94b20a56f068d784f2c4d5bb2a8586980697e6a9
Peter Nortey <ecexpay@gmail.com>
2022-04-28 12:42:26 +0000

    add event boilerplate

--- components/Modal/AddForm.js
+++ components/Modal/AddForm.js
@@ -2,16 +2,16 @@ import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { PencilIcon } from "@heroicons/react/outline";
 
-export default function AddUser({ edit, setEdit, user }) {
+export default function AddForm({ add, setAdd }) {
   const cancelButtonRef = useRef(null);
 
   return (
-    <Transition.Root show={edit} as={Fragment}>
+    <Transition.Root show={add} as={Fragment}>
       <Dialog
         as="div"
         className="fixed z-10 inset-0 overflow-y-auto"
         initialFocus={cancelButtonRef}
-        onClose={setEdit}
+        onClose={setAdd}
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
@@ -58,124 +58,7 @@ export default function AddUser({ edit, setEdit, user }) {
                     >
                       Edit account
                     </Dialog.Title>
-                    <form action="#" method="POST">
-                      <div className="shadow overflow-hidden sm:rounded-md">
-                        <div className="px-4 py-5 bg-white sm:p-6">
-                          <div className="grid grid-cols-6 gap-6">
-                            <div className="col-span-6 sm:col-span-3">
-                              <label className="block text-sm font-medium text-gray-700">
-                                Photo
-                              </label>
-                              <div className="mt-1 flex items-center">
-                                <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                  <svg
-                                    className="h-full w-full text-gray-300"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                  >
-                                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                                  </svg>
-                                </span>
-                                <button
-                                  type="button"
-                                  className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                >
-                                  Change
-                                </button>
-                              </div>
-                            </div>
-                            <div className="col-span-6 sm:col-span-6">
-                              <label
-                                htmlFor="first-name"
-                                className="block text-sm font-medium text-gray-700"
-                              >
-                                First name
-                              </label>
-                              <input
-                                type="text"
-                                name="first-name"
-                                defaultValue={user.displayName}
-                                id="first-name"
-                                autoComplete="given-name"
-                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                              />
-                            </div>
-
-                            <div className="col-span-6 sm:col-span-4">
-                              <label
-                                htmlFor="email-address"
-                                className="block text-sm font-medium text-gray-700"
-                              >
-                                Email address
-                              </label>
-                              <input
-                                type="text"
-                                name="email-address"
-                                defaultValue={user.email}
-                                id="email-address"
-                                autoComplete="email"
-                                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                              />
-                            </div>
-
-                            <div className="col-span-6 sm:col-span-3">
-                              <label
-                                htmlFor="country"
-                                className="block text-sm font-medium text-gray-700"
-                              >
-                                Company
-                              </label>
-                              <select
-                                id="country"
-                                name="country"
-                                autoComplete="country-name"
-                                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                              >
-                                <option>CHOYS</option>
-                                <option>Microsoft</option>
-                                <option>Facebook</option>
-                              </select>
-                            </div>
-
-                            <div className="col-span-6 sm:col-span-3">
-                              <label
-                                htmlFor="country"
-                                className="block text-sm font-medium text-gray-700"
-                              >
-                                Role
-                              </label>
-                              <select
-                                id="country"
-                                name="country"
-                                autoComplete="country-name"
-                                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                              >
-                                <option>Admin</option>
-                                <option>Manager</option>
-                                <option>User</option>
-                              </select>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                          <button
-                            type="submit"
-                            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                          >
-                            Save
-                          </button>
-                          <button
-                            type="button"
-                            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                            onClick={() => setEdit(false)}
-                            ref={cancelButtonRef}
-                          >
-                            Cancel
-                          </button>
-                        </div>
-                      </div>
-                    </form>
+                    {children}
                   </div>
                 </div>
               </div>
