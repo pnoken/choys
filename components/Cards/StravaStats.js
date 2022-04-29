c6d733008f547a67356b69d1b7bed30e648a771c
Peter Nortey <ecexpay@gmail.com>
2022-04-29 02:11:24 +0000

    total distance and filter strava data

--- components/Cards/StravaStats.js
+++ components/Cards/StravaStats.js
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "chart.js";
 
 export default function StravaCard({ distance, date }) {
+  const currentYear = new Date().getFullYear();
+  const [totalDistance, setTotalDistance] = useState(0);
+  const [filteredYear, setFilteredYear] = useState(currentYear);
   useEffect(() => {
+    getTotalDistance();
     window.myLine = new Chart(document.getElementById("myChart"), {
       type: "line",
       options: {
@@ -13,7 +17,7 @@ export default function StravaCard({ distance, date }) {
         labels: date,
         datasets: [
           {
-            label: "Total Distance Covered",
+            label: "Distance Covered",
             borderColor: "#4A5568",
             data: distance,
             fill: false,
@@ -28,73 +32,45 @@ export default function StravaCard({ distance, date }) {
       },
     });
   });
+
+  const getTotalDistance = () => {
+    let totDistance = distance.reduce((prevVal, curVal) => prevVal + curVal);
+    setTotalDistance(totDistance);
+  };
+
+  const yearChangeHandler = (e) => {
+    setFilteredYear(e.target.value);
+  };
   return (
     <>
       <div className="flex items-center justify-center py-8 px-4">
-        <div className="w-full">
-          <div>
-            <div className="lg:flex w-full justify-between">
-              <h3 className="text-gray-600 dark:text-gray-400 leading-5 text-base md:text-xl font-bold">
-                Total Distance Covered
-              </h3>
-              <div className="flex items-center justify-between lg:justify-start mt-2 md:mt-4 lg:mt-0">
-                {/* <div className="flex items-center">
-                    <button className="py-2 px-4 bg-gray-100 dark:bg-gray-700 focus:outline-none ease-in duration-150 text-xs text-gray-600 dark:text-gray-400 hover:bg-gray-200">
-                      Dollars
-                    </button>
-                    <button className="py-2 px-4 bg-indigo-500 focus:outline-none text-white ease-in duration-150 text-xs hover:bg-indigo-600">
-                      Tickets
-                    </button>
-                  </div> */}
-                <div className="lg:ml-14">
-                  <div className="bg-gray-100 dark:bg-gray-700 ease-in duration-150 hover:bg-gray-200 pb-2 pt-1 px-3 rounded-sm">
-                    <select className="text-xs text-gray-600 dark:text-gray-400 bg-transparent focus:outline-none">
-                      <option className="leading-1">Year</option>
-                      <option className="leading-1">2020</option>
-                      <option className="leading-1">2019</option>
-                    </select>
-                  </div>
+        <div>
+          <div className="lg:flex w-full justify-between">
+            <h3 className="text-gray-600 dark:text-gray-400 leading-5 text-base md:text-xl font-bold">
+              Total Distance Covered: {totalDistance.toFixed(2)}m
+            </h3>
+            <div className="flex items-center justify-between lg:justify-start mt-2 md:mt-4 lg:mt-0">
+              <div className="lg:ml-14">
+                <div className="bg-gray-100 dark:bg-gray-700 ease-in duration-150 hover:bg-gray-200 pb-2 pt-1 px-3 rounded-sm">
+                  <select
+                    className="text-xs text-gray-600 dark:text-gray-400 focus:outline-none"
+                    value={filteredYear}
+                    onChange={yearChangeHandler}
+                  >
+                    <option className="leading-1">Year</option>
+                    <option className="leading-1" value="2022">
+                      2022
+                    </option>
+                    <option className="leading-1" value="2021">
+                      2021
+                    </option>
+                  </select>
                 </div>
               </div>
             </div>
-            <div className="flex items-end mt-6">
-              {/* <h3 className="text-indigo-500 leading-5 text-lg md:text-2xl">
-                  $65,875
-                </h3> */}
-              <div className="flex items-center md:ml-4 ml-1">
-                {/* <p className="text-indigo-500 text-xs md:text-base">17%</p> */}
-                {/* <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                  >
-                    <path
-                      d="M6 2.5V9.5"
-                      stroke="#4338CA"
-                      strokeWidth="0.75"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M8 4.5L6 2.5"
-                      stroke="#4338CA"
-                      strokeWidth="0.75"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M4 4.5L6 2.5"
-                      stroke="#4338CA"
-                      strokeWidth="0.75"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg> */}
-              </div>
-            </div>
           </div>
+        </div>
+        <div className="w-full">
           <div className="mt-6">
             <canvas id="myChart" width="1025" height="400" />
           </div>
