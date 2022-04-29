96745c3553475873870601fca0d4e5653e5ce2ae
Peter Nortey <ecexpay@gmail.com>
2022-04-29 17:48:43 +0000

    strava data filter

--- components/Cards/StravaStats.js
+++ components/Cards/StravaStats.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Chart from "chart.js";
 
-export default function StravaCard({ distance, date }) {
-  const currentYear = new Date().getFullYear();
-  const [totalDistance, setTotalDistance] = useState(0);
-  const [filteredYear, setFilteredYear] = useState(currentYear);
+export default function StravaCard({ distance, date, getTotalDistance }) {
   useEffect(() => {
     getTotalDistance();
     window.myLine = new Chart(document.getElementById("myChart"), {
@@ -33,43 +30,9 @@ export default function StravaCard({ distance, date }) {
     });
   });
 
-  const getTotalDistance = () => {
-    let totDistance = distance.reduce((prevVal, curVal) => prevVal + curVal);
-    setTotalDistance(totDistance);
-  };
-
-  const yearChangeHandler = (e) => {
-    setFilteredYear(e.target.value);
-  };
   return (
     <>
       <div className="flex items-center justify-center py-8 px-4">
-        <div>
-          <div className="lg:flex w-full justify-between">
-            <h3 className="text-gray-600 dark:text-gray-400 leading-5 text-base md:text-xl font-bold">
-              Total Distance Covered: {totalDistance.toFixed(2)}m
-            </h3>
-            <div className="flex items-center justify-between lg:justify-start mt-2 md:mt-4 lg:mt-0">
-              <div className="lg:ml-14">
-                <div className="bg-gray-100 dark:bg-gray-700 ease-in duration-150 hover:bg-gray-200 pb-2 pt-1 px-3 rounded-sm">
-                  <select
-                    className="text-xs text-gray-600 dark:text-gray-400 focus:outline-none"
-                    value={filteredYear}
-                    onChange={yearChangeHandler}
-                  >
-                    <option className="leading-1">Year</option>
-                    <option className="leading-1" value="2022">
-                      2022
-                    </option>
-                    <option className="leading-1" value="2021">
-                      2021
-                    </option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <div className="w-full">
           <div className="mt-6">
             <canvas id="myChart" width="1025" height="400" />
--- pages/analytics.js
+++ pages/analytics.js
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react";
 import axiosInstance from "../utils/axiosStrava";
 import { formatDateTime } from "../utils/shared";
 import { useRouter } from "next/router";
-import { getAuth } from "firebase/auth";
 
 const StravaCard = dynamic(() => import("../components/Cards/StravaStats"), {
   loading: () => (
@@ -17,44 +16,93 @@ const StravaCard = dynamic(() => import("../components/Cards/StravaStats"), {
 });
 
 export default function Analytics() {
+  const currentYear = new Date().getFullYear().toString();
   const [distance, setDistance] = useState([]);
   const [date, setDate] = useState([]);
+  const [filteredYear, setFilteredYear] = useState(currentYear);
+  const [totalDistance, setTotalDistance] = useState(0);
   const router = useRouter();
-  useEffect(() => {
-    const auth = getAuth();
-    const userId = auth?.currentUser.uid;
-    console.log("user id", userId);
+
+  const getAthleteActivities = () => {
     let totalDistance = [];
     let timestamp = [];
-    const getAthleteActivities = () => {
-      axiosInstance
-        .get("/athlete/activities")
-        .then(function (response) {
-          for (let dataObj of response.data) {
-            totalDistance.unshift(dataObj.distance);
-            timestamp.unshift(formatDateTime(dataObj.start_date));
-          }
-          setDistance(totalDistance);
-          setDate(timestamp);
-        })
-        .then(() => {
-          writeUserData(userId, distance, date);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-    getAthleteActivities();
-  }, []);
+    axiosInstance
+      .get("/athlete/activities")
+      .then(function (response) {
+        for (let dataObj of filterData(response.data)) {
+          totalDistance.unshift(dataObj.distance);
+          timestamp.unshift(formatDateTime(dataObj.start_date));
+        }
+        setDistance(totalDistance);
+        setDate(timestamp);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
+  useEffect(() => {
+    getAthleteActivities();
+  }, [filteredYear]);
+
+  //filter data
+  const filterData = (stravaData) => {
+    const hey = stravaData.filter((data) => {
+      let d = new Date(data.start_date).getFullYear().toString();
+      return d === filteredYear;
+    });
+
+    return hey;
+  };
   //Write strava data to database
+
+  const yearChangeHandler = (e) => {
+    setFilteredYear(e.target.value);
+  };
+
+  const getTotalDistance = () => {
+    // if (distance.length === 0) {
+    //   return;
+    // }
+    let totDistance = distance.reduce((prevVal, curVal) => prevVal + curVal);
+
+    setTotalDistance(totDistance);
+  };
 
   return (
     <>
       <div className="flex flex-wrap">
+        <div className="lg:flex w-full justify-between">
+          <h3 className="text-gray-600 dark:text-gray-400 leading-5 text-base md:text-xl font-bold">
+            Total Distance Covered: {totalDistance.toFixed(2)}m
+          </h3>
+        </div>
+
+        <div className="w-full px-1">
+          <div className="bg-gray-100 dark:bg-gray-700 ease-in duration-150 hover:bg-gray-200 pb-2 pt-1 px-3 rounded-sm">
+            <select
+              className="text-xs text-gray-600 dark:text-gray-400 focus:outline-none"
+              value={filteredYear}
+              onChange={yearChangeHandler}
+            >
+              <option className="leading-1" value="2022">
+                2022
+              </option>
+              <option className="leading-1" value="2021">
+                2021
+              </option>
+            </select>
+          </div>
+        </div>
         <div className="w-full px-1">
           {distance && distance.length > 0 ? (
-            <StravaCard distance={distance} date={date} />
+            <StravaCard
+              distance={distance}
+              date={date}
+              filteredYear={filteredYear}
+              setFilteredYear={setFilteredYear}
+              getTotalDistance={getTotalDistance}
+            />
           ) : (
             <div>
               <h1>Strava Data not available. Please Authenticate</h1>
